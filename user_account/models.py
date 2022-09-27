0d368d07b3dd8062f4bf1e6f3730f211c246786e
moshiurshimul <hello@moshiurshimul.com>
2022-09-28 05:01:43 +0600

    'user-model'

--- user_account/models.py
+++ user_account/models.py
@@ -1,9 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from phonenumber_field.modelfields import PhoneNumberField
+from django.utils.translation import gettext_lazy as _
 
 
 class RestaurantUser(AbstractUser):
-    phone_number = PhoneNumberField(blank=False, null=False, unique=True)
+    phone_number = PhoneNumberField(null=False, blank=False, unique=True)
+    email = models.EmailField(_('Email'), blank=False, unique=True)
+
+    USERNAME_FIELD = 'phone_number'
+
+
 
 
