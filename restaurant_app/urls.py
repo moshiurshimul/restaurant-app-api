5af34664546b3e80ee6d246f4ccc747e78d317ec
moshiurshimul <hello@moshiurshimul.com>
2022-09-29 05:27:07 +0600

    JWT setup done

--- restaurant_app/urls.py
+++ restaurant_app/urls.py
@@ -14,11 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from django.urls import path, include
+# from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from user_account import urls as user_account_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/v1/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('backend/api/v1/user/', include(user_account_urls)),
 ]
