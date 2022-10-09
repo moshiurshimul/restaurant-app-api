f916f15e29342f6f8b72776b5128a956cc611dfc
moshiurshimul <hello@moshiurshimul.com>
2022-10-09 09:56:50 +0600

    menu permissions added

--- menu/views.py
+++ menu/views.py
@@ -1,4 +1,5 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAdminUser
 from rest_framework.response import Response
 from rest_framework import status
 
@@ -12,17 +13,25 @@ class MenuViewSet(ModelViewSet):
         'update': MenuUpdateSerializers,
         'partial_update': MenuUpdateSerializers,
     }
+
     default_serializer_class = MenuSerializers
     lookup_field = 'id'
 
+    # Default Serializer Class
     def get_serializer_class(self):
         return self.serializer_classes.get(self.action, self.default_serializer_class)
 
+    # Permission Class
+    def get_permissions(self):
+        if self.action in ['create', 'update', 'partial_update', 'destroy', ]:
+            self.permission_classes = [IsAdminUser, ]
+        return super().get_permissions()
+
     def update(self, request, *args, **kwargs):
         instance = self.get_object()
-        serializer = self.get_serializer(instance=instance, data=request.data)
+        serializer = self.get_serializer(instance=instance, data=request.data, partial=True)
         if serializer.is_valid():
-            serializer.update(instance=instance, validated_data=serializer.validated_data)
+            self.perform_update(serializer)
             return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(serializer.error, status=status.HTTP_400_BAD_REQUEST)
 
