931b9f888597f87b0bbf3bc412b59e2fd2b51c43
moshiurshimul <hello@moshiurshimul.com>
2022-10-10 07:17:21 +0600

    admin update

--- menu/admin.py
+++ menu/admin.py
@@ -5,5 +5,5 @@ from menu.models import Menu
 
 @admin.register(Menu)
 class MenuAdmin(admin.ModelAdmin):
-    list_display = ('title', 'short_details', 'available_time', 'ability_today', 'price')
-
+    list_display = ('title', 'short_details', 'available_time', 'ability_today', 'price', )
+    list_filter = ('available_time', 'ability_today', )
